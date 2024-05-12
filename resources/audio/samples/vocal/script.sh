1c5238f5612e5f0eb09a2c481a5539f577164f7e
yagyudar <actsofgolgom@gmail.com>
2024-05-12 20:59:48 +0900

    ボーカル用のサンプルwavを追加

---
+++ resources/audio/samples/vocal/script.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+CHARACTER_READ=$1
+CHARACTER_DIR=$1
+
+# rm -rf ./${CHARACTER_DIR}
+# mkdir ./${CHARACTER_DIR}
+
+KEY=52
+
+QUERY=`curl -s -H "Content-Type: application/json" -j POST "127.0.0.1:50021/sing_frame_audio_query?speaker=6000" -d @- <<EOS
+{ "notes": [ { "key": null, "frame_length": 3, "lyric": "" }, { "key": ${KEY}, "frame_length": 45, "lyric": "${CHARACTER_READ}" } ] }
+EOS`
+curl -s -H "Content-Type: application/json" -X POST "127.0.0.1:50021/frame_synthesis?speaker=3001" -d @- > ./${CHARACTER_DIR}${KEY}.wav <<EOS
+${QUERY}
+EOS
+
+KEY=57
+
+QUERY=`curl -s -H "Content-Type: application/json" -j POST "127.0.0.1:50021/sing_frame_audio_query?speaker=6000" -d @- <<EOS
+{ "notes": [ { "key": null, "frame_length": 3, "lyric": "" }, { "key": ${KEY}, "frame_length": 45, "lyric": "${CHARACTER_READ}" } ] }
+EOS`
+curl -s -H "Content-Type: application/json" -X POST "127.0.0.1:50021/frame_synthesis?speaker=3001" -d @- > ./${CHARACTER_DIR}${KEY}.wav <<EOS
+${QUERY}
+EOS
+
+KEY=64
+
+QUERY=`curl -s -H "Content-Type: application/json" -j POST "127.0.0.1:50021/sing_frame_audio_query?speaker=6000" -d @- <<EOS
+{ "notes": [ { "key": null, "frame_length": 3, "lyric": "" }, { "key": ${KEY}, "frame_length": 45, "lyric": "${CHARACTER_READ}" } ] }
+EOS`
+curl -s -H "Content-Type: application/json" -X POST "127.0.0.1:50021/frame_synthesis?speaker=3001" -d @- > ./${CHARACTER_DIR}${KEY}.wav <<EOS
+${QUERY}
+EOS
+
+KEY=69
+
+QUERY=`curl -s -H "Content-Type: application/json" -j POST "127.0.0.1:50021/sing_frame_audio_query?speaker=6000" -d @- <<EOS
+{ "notes": [ { "key": null, "frame_length": 3, "lyric": "" }, { "key": ${KEY}, "frame_length": 45, "lyric": "${CHARACTER_READ}" } ] }
+EOS`
+curl -s -H "Content-Type: application/json" -X POST "127.0.0.1:50021/frame_synthesis?speaker=3001" -d @- > ./${CHARACTER_DIR}${KEY}.wav <<EOS
+${QUERY}
+EOS
+
+KEY=76
+
+QUERY=`curl -s -H "Content-Type: application/json" -j POST "127.0.0.1:50021/sing_frame_audio_query?speaker=6000" -d @- <<EOS
+{ "notes": [ { "key": null, "frame_length": 3, "lyric": "" }, { "key": ${KEY}, "frame_length": 45, "lyric": "${CHARACTER_READ}" } ] }
+EOS`
+curl -s -H "Content-Type: application/json" -X POST "127.0.0.1:50021/frame_synthesis?speaker=3001" -d @- > ./${CHARACTER_DIR}${KEY}.wav <<EOS
+${QUERY}
+EOS
+
