708615790e121fd54265e9447e8ed5877eb79aff
yagyudar <actsofgolgom@gmail.com>
2024-05-16 23:22:03 +0900

    サンプリング音声を調整

--- docs/resources/audio/samples/vocal/main.sh
+++ docs/resources/audio/samples/vocal/main.sh
@@ -1,113 +1,118 @@
 #!/bin/bash
 
-# sh ./script.sh あ
-# sh ./script.sh い
-# sh ./script.sh う
-# sh ./script.sh え
-# sh ./script.sh お
-# sh ./script.sh か
-# sh ./script.sh き
-# sh ./script.sh く
-# sh ./script.sh け
-# sh ./script.sh こ
-# sh ./script.sh さ
-# sh ./script.sh し
-# sh ./script.sh す
-# sh ./script.sh せ
-# sh ./script.sh そ
-# sh ./script.sh た
-# sh ./script.sh ち
-# sh ./script.sh つ
-# sh ./script.sh て
-# sh ./script.sh と
-# sh ./script.sh な
-# sh ./script.sh に
-# sh ./script.sh ぬ
-# sh ./script.sh ね
-# sh ./script.sh の
-# sh ./script.sh は
-# sh ./script.sh ひ
-# sh ./script.sh ふ
-# sh ./script.sh へ
-# sh ./script.sh ほ
-# sh ./script.sh ま
-# sh ./script.sh み
-# sh ./script.sh む
-# sh ./script.sh め
-# sh ./script.sh も
-# sh ./script.sh が
-# sh ./script.sh ぎ
-# sh ./script.sh ぐ
-# sh ./script.sh げ
-# sh ./script.sh ご
-# sh ./script.sh ざ
-# sh ./script.sh じ
-# sh ./script.sh ず
-# sh ./script.sh ぜ
-# sh ./script.sh ぞ
-# sh ./script.sh だ
-# sh ./script.sh ぢ
-# sh ./script.sh づ
-# sh ./script.sh で
-# sh ./script.sh ど
-# sh ./script.sh ば
-# sh ./script.sh び
-# sh ./script.sh ぶ
-# sh ./script.sh べ
-# sh ./script.sh ぼ
-# sh ./script.sh ぱ
-# sh ./script.sh ぴ
-# sh ./script.sh ぷ
-# sh ./script.sh ぺ
-# sh ./script.sh ぽ
-# sh ./script.sh や
-# sh ./script.sh ゆ
-# sh ./script.sh よ
-# sh ./script.sh わ
-# sh ./script.sh を
-# sh ./script.sh ん
-# sh ./script.sh きゃ
-# sh ./script.sh しゃ
-# sh ./script.sh ちゃ
-# sh ./script.sh にゃ
-# sh ./script.sh ひゃ
-# sh ./script.sh みゃ
-# sh ./script.sh ぎゃ
-# sh ./script.sh じゃ
-# # sh ./script.sh ぢゃ
-# sh ./script.sh びゃ
-# sh ./script.sh ぴゃ
-# sh ./script.sh きゅ
-# sh ./script.sh しゅ
-# sh ./script.sh ちゅ
-# sh ./script.sh にゅ
-# sh ./script.sh ひゅ
-# sh ./script.sh みゅ
-# sh ./script.sh ぎゅ
-# sh ./script.sh じゅ
-# # sh ./script.sh ぢゅ
-# sh ./script.sh びゅ
-# sh ./script.sh ぴゅ
-# sh ./script.sh きょ
-# sh ./script.sh しょ
-# sh ./script.sh ちょ
-# sh ./script.sh にょ
-# sh ./script.sh ひょ
-# sh ./script.sh みょ
-# sh ./script.sh ぎょ
-# sh ./script.sh じょ
-# # sh ./script.sh ぢょ
-# sh ./script.sh びょ
-# sh ./script.sh ぴょ
-# sh ./script.sh ふぁ
-# sh ./script.sh ふぃ
-# sh ./script.sh ふぇ
-# sh ./script.sh ふぉ
-# # sh ./script.sh ぶぁ
-# # sh ./script.sh ぶぃ
-# # sh ./script.sh ぶぇ
-# # sh ./script.sh ぶぉ
+SH ./SCRIPT.SH あ
+sh ./script.sh い
+sh ./script.sh う
+sh ./script.sh え
+sh ./script.sh お
+sh ./script.sh か
+sh ./script.sh き
+sh ./script.sh く
+sh ./script.sh け
+sh ./script.sh こ
+sh ./script.sh さ
+sh ./script.sh し
+sh ./script.sh す
+sh ./script.sh せ
+sh ./script.sh そ
+sh ./script.sh た
+sh ./script.sh ち
+sh ./script.sh つ
+sh ./script.sh て
+sh ./script.sh と
+sh ./script.sh な
+sh ./script.sh に
+sh ./script.sh ぬ
+sh ./script.sh ね
+sh ./script.sh の
+sh ./script.sh は
+sh ./script.sh ひ
+sh ./script.sh ふ
+sh ./script.sh へ
+sh ./script.sh ほ
+sh ./script.sh ま
+sh ./script.sh み
+sh ./script.sh む
+sh ./script.sh め
+sh ./script.sh も
+sh ./script.sh が
+sh ./script.sh ぎ
+sh ./script.sh ぐ
+sh ./script.sh げ
+sh ./script.sh ご
+sh ./script.sh ざ
+sh ./script.sh じ
+sh ./script.sh ず
+sh ./script.sh ぜ
+sh ./script.sh ぞ
+sh ./script.sh だ
+sh ./script.sh ぢ
+sh ./script.sh づ
+sh ./script.sh で
+sh ./script.sh ど
+sh ./script.sh ば
+sh ./script.sh び
+sh ./script.sh ぶ
+sh ./script.sh べ
+sh ./script.sh ぼ
+sh ./script.sh ぱ
+sh ./script.sh ぴ
+sh ./script.sh ぷ
+sh ./script.sh ぺ
+sh ./script.sh ぽ
+sh ./script.sh や
+sh ./script.sh ゆ
+sh ./script.sh よ
+sh ./script.sh わ
+sh ./script.sh を
+sh ./script.sh ん
+sh ./script.sh きゃ
+sh ./script.sh しゃ
+sh ./script.sh ちゃ
+sh ./script.sh にゃ
+sh ./script.sh ひゃ
+sh ./script.sh みゃ
+sh ./script.sh ぎゃ
+sh ./script.sh じゃ
+# sh ./script.sh ぢゃ
+sh ./script.sh びゃ
+sh ./script.sh ぴゃ
+sh ./script.sh きゅ
+sh ./script.sh しゅ
+sh ./script.sh ちゅ
+sh ./script.sh にゅ
+sh ./script.sh ひゅ
+sh ./script.sh みゅ
+sh ./script.sh ぎゅ
+sh ./script.sh じゅ
+# sh ./script.sh ぢゅ
+sh ./script.sh びゅ
+sh ./script.sh ぴゅ
+sh ./script.sh きょ
+sh ./script.sh しょ
+sh ./script.sh ちょ
+sh ./script.sh にょ
+sh ./script.sh ひょ
+sh ./script.sh みょ
+sh ./script.sh ぎょ
+sh ./script.sh じょ
+# sh ./script.sh ぢょ
+sh ./script.sh びょ
+sh ./script.sh ぴょ
+sh ./script.sh ふぁ
+sh ./script.sh ふぃ
+sh ./script.sh ふぇ
+sh ./script.sh ふぉ
+# sh ./script.sh ぶぁ
+# sh ./script.sh ぶぃ
+# sh ./script.sh ぶぇ
+# sh ./script.sh ぶぉ
 sh ./script.sh ヴァ
 sh ./script.sh ヴィ
 sh ./script.sh ヴェ
 sh ./script.sh ヴォ
+sh ./script.sh ら
+sh ./script.sh り
+sh ./script.sh る
+sh ./script.sh れ
+sh ./script.sh ろ
